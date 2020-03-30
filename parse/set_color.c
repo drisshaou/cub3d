23fa22bf9e4cb8ea1841a8145c6db299fe27a36d
driss haouhaou <driss@MacBook-Pro-de-driss-6.local>
2020-03-31 01:02:24 +0200

    cub3d last update

--- parse/set_color.c
+++ parse/set_color.c
@@ -6,37 +6,43 @@
 /*   By: dhaouhao <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/13 04:17:17 by dhaouhao          #+#    #+#             */
-/*   Updated: 2020/03/31 00:44:40 by dhaouhao         ###   ########.fr       */
+/*   Updated: 2020/03/31 01:01:08 by dhaouhao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	rgb_int(int r, int g, int b)
+int		rgb_int(int r, int g, int b)
 {
 	return (r << 16) | (g << 8) | b;
 }
 
-int	set_color(t_c3d *t, char **split)
+void	fill_color(t_c3d *t, char *str, char **rgb)
+{
+	int		rgb_i;
+
+	rgb_i = rgb_int(ft_atoi(rgb[0]), ft_atoi(rgb[1]), ft_atoi(rgb[2]));
+	t->flr_color = !ft_strcmp(str, "F") ? rgb_i : t->flr_color;
+	t->cl_color = !ft_strcmp(str, "C") ? rgb_i : t->cl_color;
+
+}
+
+int		set_color(t_c3d *t, char **split)
 {
 	char	**rgb;
 	int		i;
 	int		j;
-	int		rgb_i;
 	int		ret;
 
-	ret = (rgb = ft_split(split[1], ',')) == 3 ? 1 : 0;
+	ret = 1;
+	rgb = ft_split(split[1], ',');
 	if (ft_get_tab_size((rgb)) == 3 && (i = -1))
 	{
 		while (++i < 3 && (j = -1))
 			while (rgb[i][++j])
 				ret = !ft_isdigit(rgb[i][j]) || ft_atoi(rgb[i]) > 255 ? 0 : ret;
 		if (ret == 1)
-		{
-			rgb_i = rgb_int(ft_atoi(rgb[0]), ft_atoi(rgb[1]), ft_atoi(rgb[2]));
-			t->flr_color = !ft_strcmp(split[0], "F") ? rgb_i : t->flr_color;
-			t->cl_color = !ft_strcmp(split[0], "C") ? rgb_i : t->cl_color;
-		}
+			fill_color(t, split[0], rgb);
 	}
 	else if (!set_texture(t, split))
 		ret = -1;
